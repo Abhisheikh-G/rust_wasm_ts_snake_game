45ea61a3eb2aba1edc9b3d3b311718dd414cb4ad
abhisheikh <abhisheikhg@gmail.com>
2022-01-30 18:42:26 -0500

    Complete game, prepare for deployment

--- src/lib.rs
+++ src/lib.rs
@@ -5,6 +5,11 @@ use wee_alloc::WeeAlloc;
 #[global_allocator]
 static ALLOC: WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
+#[wasm_bindgen(module = "/www/utils/random.js")]
+extern {
+    fn random(max: usize) -> usize;
+}
+
 #[wasm_bindgen]
 #[derive(PartialEq)]
 pub enum Direction {
@@ -14,7 +19,15 @@ pub enum Direction {
     Left
 }
 
+#[wasm_bindgen]
 #[derive(Clone, Copy)]
+pub enum GameStatus {
+    Won,
+    Lost,
+    Played
+}
+
+#[derive(Clone, Copy, PartialEq)]
 pub struct SnakeCell(usize);
 
 struct Snake {
@@ -41,18 +54,51 @@ pub struct World {
      width: usize,
      snake: Snake,
      size: usize,
-     next_cell: Option<SnakeCell>
+     next_cell: Option<SnakeCell>,
+     reward_cell: Option<usize>,
+     status: Option<GameStatus>,
+     points: usize
 } 
 
 #[wasm_bindgen]
 impl World {
     pub fn new(width: usize, snake_idx: usize) -> World {
+        let snake = Snake::new(snake_idx, 3);
+        let size = width * width;
+
+        let mut reward_cell;
+        loop {
+            reward_cell = random(size);
+            if !snake.body.contains(&SnakeCell(reward_cell)) { break; } 
+        }
+
         World {
             width,
-            size: width * width,
-            snake: Snake::new(snake_idx, 3),
-            next_cell: None
+            size,
+            reward_cell: World::gen_reward_cell(size, &snake.body),
+            snake,
+            next_cell: None,
+            status: None,
+            points: 0
         }
+    }
+
+    fn gen_reward_cell(max: usize, snake_body: &Vec<SnakeCell>) -> Option<usize>{
+        let mut reward_cell;
+        loop {
+            reward_cell = random(max);
+            if !snake_body.contains(&SnakeCell(reward_cell)) { break; } 
+        }
+        
+        Some(reward_cell)
+    }
+
+    pub fn points(&self) -> usize {
+        self.points
+    }
+
+    pub fn reward_cell(&self) -> Option<usize> {
+        self.reward_cell
     }
 
     pub fn width(&self) -> usize {
@@ -65,6 +111,23 @@ impl World {
 
     pub fn world_size(&self) -> usize {
         self.size
+    }
+
+    pub fn start_game(&mut self) {
+        self.status = Some(GameStatus::Played);
+    }
+
+    pub fn game_status(&self) -> Option<GameStatus>{
+        self.status
+    }
+
+    pub fn game_status_text(&self) -> String {
+        match self.status {
+            Some(GameStatus::Won)  => String::from("You have won!"),
+            Some(GameStatus::Lost)  => String::from("You have lost!"),
+            Some(GameStatus::Played)  => String::from("Game in progress.."),
+            None  => String::from("Ready to play?"),
+        }
     }
 
     pub fn change_snake_dir(&mut self, direction: Direction){
@@ -84,23 +147,44 @@ impl World {
     }
 
     pub fn step(&mut self) {
-        let temp = self.snake.body.clone();
+        match self.status {
+            Some(GameStatus::Played) => {
+                let temp = self.snake.body.clone();
 
-        match self.next_cell {
-            Some(cell) => {
-                self.snake.body[0] = cell;
-                self.next_cell = None;
+                match self.next_cell {
+                    Some(cell) => {
+                        self.snake.body[0] = cell;
+                        self.next_cell = None;
+                    },
+                    None=> {
+                        self.snake.body[0] = self.gen_next_snake_cell(&self.snake.direction);
+                    }
+                }
+        
+                for i in 1..self.snake_length() {
+                    self.snake.body[i] = SnakeCell(temp[i -1].0);
+                }
+
+                if self.snake.body[1..self.snake_length()].contains(&self.snake.body[0]) {
+                    self.status = Some(GameStatus::Lost)
+                }
+        
+                if self.reward_cell == Some(self.snake_head_idx()) {
+                    if self.snake_length() < self.size {
+                        self.points += 1;
+                        self.reward_cell = World::gen_reward_cell(self.size, &self.snake.body)
+                    } else {
+                        self.reward_cell = None;
+                        self.status = Some(GameStatus::Won);
+                    }
+                    self.snake.body.push(SnakeCell(self.snake.body[1].0));   
+                }
             },
-            None=> {
-                self.snake.body[0] = self.gen_next_snake_cell(&self.snake.direction);
-            }
+            _ => {}
         }
 
-        let snake_len = self.snake.body.len();
 
-        for i in 1..snake_len {
-            self.snake.body[i] = SnakeCell(temp[i -1].0);
-        }
+        
     }
 
     fn gen_next_snake_cell(&self, direction: &Direction) -> SnakeCell {
--- www/index.ts
+++ www/index.ts
@@ -1,39 +1,57 @@
-import init, { World, Direction } from "snake_game";
+import init, { World, Direction, GameStatus } from "snake_game";
 
-init().then(wasm => {
+init().then((wasm) => {
   const CELL_SIZE = 30;
-  const WORLD_WIDTH = 8;
+  const WORLD_WIDTH = 12;
   const SNAKE_SPAWN_IDX = Date.now() % (WORLD_WIDTH * WORLD_WIDTH);
 
   const world = World.new(WORLD_WIDTH, SNAKE_SPAWN_IDX);
-  
-  
+
+  const gameContrlBtn = document.getElementById("game-control-btn");
+  const gameStatusEl = document.getElementById("game-status");
+  const gamePointsEl = document.getElementById("game-points");
+
   const canvas = <HTMLCanvasElement>document.getElementById("snake-canvas");
   const ctx = canvas.getContext("2d");
   canvas.height = WORLD_WIDTH * CELL_SIZE;
   canvas.width = WORLD_WIDTH * CELL_SIZE;
 
-  const snakeCellPtr = world.snake_cells();
-  const snakeLength = world.snake_length();
-
-
+  gameContrlBtn.addEventListener("click", (_) => {
+    const status = world.game_status();
+    if (status === undefined) {
+      world.start_game();
+      play();
+      gameContrlBtn.innerHTML = "In Progress..";
+    } else {
+      location.reload();
+    }
+  });
 
   document.addEventListener("keydown", (e) => {
-    switch(e.code) {
+    switch (e.code) {
       case "ArrowUp":
-        world.change_snake_dir(Direction.Up)
+        world.change_snake_dir(Direction.Up);
         break;
       case "ArrowDown":
-        world.change_snake_dir(Direction.Down)
+        world.change_snake_dir(Direction.Down);
         break;
       case "ArrowLeft":
-        world.change_snake_dir(Direction.Left)
+        world.change_snake_dir(Direction.Left);
         break;
       case "ArrowRight":
-        world.change_snake_dir(Direction.Right)
+        world.change_snake_dir(Direction.Right);
         break;
     }
-  })
+  });
+
+  function drawGameStatus() {
+    gameStatusEl.textContent = world.game_status_text();
+    gamePointsEl.textContent = world.points().toString();
+  }
+
+  function drawGamePoints() {
+    
+  }
 
   function drawWorld() {
     ctx.beginPath();
@@ -53,36 +71,60 @@ init().then(wasm => {
       wasm.memory.buffer,
       world.snake_cells(),
       world.snake_length()
-    )
+    );
 
-    snakeCells.forEach((cellIdx, i) => {
-      const col = cellIdx % WORLD_WIDTH;
-      const row = Math.floor(cellIdx / WORLD_WIDTH);
+    snakeCells
+      .filter((cellIdx, i) => !(i > 0 && cellIdx === snakeCells[0]))
+      .forEach((cellIdx, i) => {
+        const col = cellIdx % WORLD_WIDTH;
+        const row = Math.floor(cellIdx / WORLD_WIDTH);
 
-      ctx.fillStyle = i === 0 ? "#7878db" : "#000000"
-      ctx.beginPath();
-      ctx.fillRect(col * CELL_SIZE, row * CELL_SIZE, CELL_SIZE, CELL_SIZE);
-    })
+        ctx.fillStyle = i === 0 ? "#7878db" : "#000000";
+        ctx.beginPath();
+        ctx.fillRect(col * CELL_SIZE, row * CELL_SIZE, CELL_SIZE, CELL_SIZE);
+      });
 
- 
     ctx.stroke();
+  }
+
+  function drawReward() {
+    const rewardIdx = world.reward_cell();
+    console.log(rewardIdx);
+    const col = rewardIdx % WORLD_WIDTH;
+    const row = Math.floor(rewardIdx / WORLD_WIDTH);
+
+    ctx.beginPath();
+    ctx.fillStyle = "#FF0000";
+    ctx.fillRect(col * CELL_SIZE, row * CELL_SIZE, CELL_SIZE, CELL_SIZE);
+    ctx.stroke();
+
+    if (rewardIdx === 1000) {
+      alert("You won!");
+    }
   }
 
   function paint() {
     drawWorld();
     drawSnake();
+    drawReward();
+    drawGameStatus();
+    drawGamePoints()
   }
 
-  function update() {
-    const fps = 3;
+  function play() {
+    const status = world.game_status();
+    if(status === GameStatus.Won || status == GameStatus.Lost){
+      gameContrlBtn.innerHTML = "Play Again?";
+      return;
+    }
+    const fps = 6;
     setTimeout(() => {
       ctx.clearRect(0, 0, canvas.width, canvas.height);
       world.step();
       paint();
-      requestAnimationFrame(update);
+      requestAnimationFrame(play);
     }, 1000 / fps);
   }
 
   paint();
-  update();
 });
