b102841a0020a9c60fd4dd16292b31c6caf6396b
abhisheikh <abhisheikhg@gmail.com>
2022-01-30 18:59:40 -0500

    Prepare node server for deployment

---
+++ server/index.js
@@ -0,0 +1,20 @@
+const compression = require("compression");
+const express = require("express");
+const path = require("path");
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+const public = path.join(__dirname, "..", "www", "public");
+
+app.use(compression());
+
+app.use(express.static(public));
+
+app.get("*", (req, res) => {
+  res.sendFile(public + "/index.html");
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
