ba0a18baf5be3866e8331e7406d0381f47fb5811
abhisheikh <abhisheikhg@gmail.com>
2022-01-30 15:24:09 -0500

    Add multiple snake cells, display additional cells, refactor code

--- src/lib.rs
+++ src/lib.rs
@@ -5,15 +5,17 @@ use wee_alloc::WeeAlloc;
 #[global_allocator]
 static ALLOC: WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
+#[wasm_bindgen]
 #[derive(PartialEq)]
-enum Direction {
+pub enum Direction {
     Up,
     Right,
     Down,
     Left
 }
 
-struct SnakeCell(usize);
+#[derive(Clone, Copy)]
+pub struct SnakeCell(usize);
 
 struct Snake {
     body: Vec<SnakeCell>,
@@ -21,10 +23,15 @@ struct Snake {
 }
 
 impl Snake {
-    fn new(spawn_index: usize) -> Snake {
+    fn new(spawn_index: usize, size: usize) -> Snake {
+
+        let mut body = vec!();
+        for i in 0..size {
+            body.push(SnakeCell(spawn_index - i))
+        }
         Snake {
-            body: vec!(SnakeCell(spawn_index)),
-            direction: Direction::Up
+            body,
+            direction: Direction::Right
         }
     }
 }
@@ -33,7 +40,8 @@ impl Snake {
 pub struct World {
      width: usize,
      snake: Snake,
-     size: usize
+     size: usize,
+     next_cell: Option<SnakeCell>
 } 
 
 #[wasm_bindgen]
@@ -42,7 +50,8 @@ impl World {
         World {
             width,
             size: width * width,
-            snake: Snake::new(snake_idx)
+            snake: Snake::new(snake_idx, 3),
+            next_cell: None
         }
     }
 
@@ -58,34 +67,80 @@ impl World {
         self.size
     }
 
-    pub fn update(&mut self) {
-        let snake_idx = self.snake_head_idx();
-        let (row, col) = self.index_to_cell(snake_idx);
-        let (row, col) = match self.snake.direction {
-            Direction::Right => {
-                (row, (col + 1) % self.width)
-            },
-            Direction::Left => {
-                (row, (col - 1) % self.width)
-            },
-            Direction::Up => {
-                ((row - 1) % self.width, col)
-            },
-            Direction::Down => {
-                ((row + 1) % self.width, col)
-            },
-        };
-        let next_idx = self.cell_to_idx(row, col);
-        self.set_snake_head(next_idx);
+    pub fn change_snake_dir(&mut self, direction: Direction){
+        let next_cell = self.gen_next_snake_cell(&direction);
+        if self.snake.body[1].0 == next_cell.0 { return; }
+
+        self.next_cell = Some(next_cell);
+        self.snake.direction = direction;
     }
 
-    fn set_snake_head(&mut self, idx: usize) {
-        self.snake.body[0].0 = idx;
+    pub fn snake_cells(&self) -> *const SnakeCell {
+        self.snake.body.as_ptr()
     }
-    fn index_to_cell(&self, idx: usize) -> (usize, usize) {
-        (idx / self.width, idx % self.width)
+
+    pub fn snake_length(&self) -> usize {
+        self.snake.body.len()
     }
-    fn cell_to_idx(&self, row: usize, col: usize) -> usize {
-        (row * self.width) + col
+
+    pub fn step(&mut self) {
+        let temp = self.snake.body.clone();
+
+        match self.next_cell {
+            Some(cell) => {
+                self.snake.body[0] = cell;
+                self.next_cell = None;
+            },
+            None=> {
+                self.snake.body[0] = self.gen_next_snake_cell(&self.snake.direction);
+            }
+        }
+
+        let snake_len = self.snake.body.len();
+
+        for i in 1..snake_len {
+            self.snake.body[i] = SnakeCell(temp[i -1].0);
+        }
+    }
+
+    fn gen_next_snake_cell(&self, direction: &Direction) -> SnakeCell {
+        let snake_idx = self.snake_head_idx();
+        let row = snake_idx / self.width;
+
+       
+        return match direction {
+            Direction::Right => {
+                let threshold = (row + 1) * self.width;
+                if snake_idx + 1 == threshold {
+                    SnakeCell(threshold - self.width)
+                } else {
+                    SnakeCell(snake_idx + 1)
+                }
+            },
+            Direction::Left => {
+                let threshold = row  * self.width;
+                if snake_idx == threshold {
+                    SnakeCell(threshold + (self.width - 1))
+                } else {
+                    SnakeCell(snake_idx - 1)
+                }
+            },
+            Direction::Up => {
+                let threshold = snake_idx - (row * self.width);
+                if snake_idx == threshold {
+                    SnakeCell((self.size - self.width) + threshold)
+                } else {
+                    SnakeCell(snake_idx - self.width)
+                }
+            },
+            Direction::Down => {
+                let threshold = snake_idx + ((self.width - row) * self.width);
+                if snake_idx + self.width == threshold {
+                    SnakeCell(threshold - ((row + 1) * self.width))
+                } else {
+                    SnakeCell(snake_idx + self.width)
+                }
+            },
+        };
     }
 }
--- www/index.ts
+++ www/index.ts
@@ -1,6 +1,6 @@
-import init, { World } from "snake_game";
+import init, { World, Direction } from "snake_game";
 
-init().then((_) => {
+init().then(wasm => {
   const CELL_SIZE = 30;
   const WORLD_WIDTH = 8;
   const SNAKE_SPAWN_IDX = Date.now() % (WORLD_WIDTH * WORLD_WIDTH);
@@ -12,6 +12,28 @@ init().then((_) => {
   const ctx = canvas.getContext("2d");
   canvas.height = WORLD_WIDTH * CELL_SIZE;
   canvas.width = WORLD_WIDTH * CELL_SIZE;
+
+  const snakeCellPtr = world.snake_cells();
+  const snakeLength = world.snake_length();
+
+
+
+  document.addEventListener("keydown", (e) => {
+    switch(e.code) {
+      case "ArrowUp":
+        world.change_snake_dir(Direction.Up)
+        break;
+      case "ArrowDown":
+        world.change_snake_dir(Direction.Down)
+        break;
+      case "ArrowLeft":
+        world.change_snake_dir(Direction.Left)
+        break;
+      case "ArrowRight":
+        world.change_snake_dir(Direction.Right)
+        break;
+    }
+  })
 
   function drawWorld() {
     ctx.beginPath();
@@ -27,12 +49,22 @@ init().then((_) => {
   }
 
   function drawSnake() {
-    const snakeIdx = world.snake_head_idx();
-    const col = snakeIdx % WORLD_WIDTH;
-    const row = Math.floor(snakeIdx / WORLD_WIDTH);
+    const snakeCells = new Uint32Array(
+      wasm.memory.buffer,
+      world.snake_cells(),
+      world.snake_length()
+    )
 
-    ctx.beginPath();
-    ctx.fillRect(col * CELL_SIZE, row * CELL_SIZE, CELL_SIZE, CELL_SIZE);
+    snakeCells.forEach((cellIdx, i) => {
+      const col = cellIdx % WORLD_WIDTH;
+      const row = Math.floor(cellIdx / WORLD_WIDTH);
+
+      ctx.fillStyle = i === 0 ? "#7878db" : "#000000"
+      ctx.beginPath();
+      ctx.fillRect(col * CELL_SIZE, row * CELL_SIZE, CELL_SIZE, CELL_SIZE);
+    })
+
+ 
     ctx.stroke();
   }
 
@@ -45,7 +77,7 @@ init().then((_) => {
     const fps = 3;
     setTimeout(() => {
       ctx.clearRect(0, 0, canvas.width, canvas.height);
-      world.update();
+      world.step();
       paint();
       requestAnimationFrame(update);
     }, 1000 / fps);
